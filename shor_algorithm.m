4522f324b2c25311ae530e1e6525740e153f4f4a
Om Joshi <omajoshi9@gmail.com>
2021-11-19 03:48:02 -0600

    more bug fixes

--- shor_algorithm.m
+++ shor_algorithm.m
@@ -2,66 +2,72 @@
 % Shor's Algorithm, implemented in Matlab
 
 N = input("N = ");
-x = input("x = ");
-if gcd(N,x) ~= 1 
-    fprintf("Hooray! gcd(N,x) != 1, it's %d which is a factor of %d.\n",gcd(N,x),N);
-    return;
-end
-Q = N^2;
-qft = zeros(Q,Q);
-for i = 1:Q
-    for j = 1:Q
-        qft(i,j) = exp(1i*2*pi*(i-1)*(j-1)/Q)/sqrt(Q);
+
+while true
+    x = input("x = ");
+    if gcd(N,x) ~= 1 
+        fprintf("Hooray! gcd(N,x) != 1, it's %d which is a factor of %d.\n",gcd(N,x),N);
+        return;
     end
-end
-
-r = ones(Q,1)/sqrt(Q);
-
-r(1) = x^(1-0);
-for i = 2:Q
-    r(i) = mod(x*r(i-1),N);
-end
-
-% fprintf("These are the values of f(r): \n")
-% disp(unique(r.'));
-% f_r = input("Pick one to 'measure': ");
-% fprintf("\n\n\n");
-f_r = 1;
-
-for i = 1:Q 
-    if r(i) ~= f_r
-        r(i) = 0;
+    Q = N^2;
+    qft = zeros(Q,Q);
+    for i = 1:Q
+        for j = 1:Q
+            qft(i,j) = exp(1i*2*pi*(i-1)*(j-1)/Q)/sqrt(Q);
+        end
     end
-end
-    
-r = normalize(r, 'norm');
 
-qft_r = qft*r;
-figure;
-plot(0:Q-1,abs(qft_r).^2);
-xlim([-Q/10 Q+Q/10]);
-title("QFT for N=" + N + " Q = " + Q + " x=" + x);
-xlabel("k");
-ylabel("P(k)");
-[sorted,indices] = sort(qft_r, 'descend');
+    r = ones(Q,1)/sqrt(Q);
 
-
-
-top = 10;
-ks = zeros(1,top);
-for i = 1 : top
-    if abs(sorted(i)) > 1e-6
-        ks(1,i) = (indices(i)-1)/Q;
-        % fprintf("state |%d> with probability %f\n", indices(i)-1, abs(sorted(i)).^2);
+    r(1) = x^(1-0);
+    for i = 2:Q
+        r(i) = mod(x*r(i-1),N);
     end
+
+    % fprintf("These are the values of f(r): \n")
+    % disp(unique(r.'));
+    % f_r = input("Pick one to 'measure': ");
+    % fprintf("\n\n\n");
+    f_r = 1;
+
+    for i = 1:Q 
+        if r(i) ~= f_r
+            r(i) = 0;
+        end
+    end
+
+    r = normalize(r, 'norm');
+
+    qft_r = qft*r;
+    figure;
+    plot(0:Q-1,abs(qft_r).^2);
+    xlim([-Q/10 Q+Q/10]);
+    title("QFT for N=" + N + " Q = " + Q + " x=" + x);
+    xlabel("k");
+    ylabel("P(k)");
+    [sorted,indices] = sort(qft_r, 'descend');
+
+
+
+    top = 10;
+    ks = zeros(1,top);
+    for i = 1 : top
+        if abs(sorted(i)) > 1e-6
+            ks(1,i) = (indices(i)-1)/Q;
+            % fprintf("state |%d> with probability %f\n", indices(i)-1, abs(sorted(i)).^2);
+        end
+    end
+    [ks,indices] = sort(ks);
+
+    % k = input("above are the top few states and their amplitudes. pick one to 'measure' as 'k': ");
+    % fprintf("k/Q = %d/%d = %f\n", k,Q, k/Q);
+
+    fprintf("These are the approximate values of c/s so far:\n");
+    disp(ks);
+    s = input("What do you think s is? ");
+    if mod(s,2) == 0 && mod(x^(s/2),N) ~= -1
+        fprintf("If everything went right, %d = %d x %d\n", N, gcd(N,x^(s/2)-1), gcd(N,x^(s/2)+1));
+        return;
+    end
+    fprintf("You were unlucky! Try again with a different x value!\n");
 end
-[ks,indices] = sort(ks);
-
-% k = input("above are the top few states and their amplitudes. pick one to 'measure' as 'k': ");
-% fprintf("k/Q = %d/%d = %f\n", k,Q, k/Q);
-
-fprintf("These are the approximate values of c/s so far:\n");
-disp(ks);
-s = input("What do you think s is? ");
-
-fprintf("The two factors of %d are %d and %d\n", N, gcd(N,x^(s/2)-1), gcd(N,x^(s/2)+1));
